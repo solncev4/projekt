769490b4b43b8bbc16c1ddb98f6b3d1b30d3b515
solncev4 <solncevvova27@gmail.com>
2021-12-05 16:26:58 +0700

    vova 999999

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,14 @@
 fun main() {//Главная функция
-    For_Test() // Зовем побочную функцию
+    Numbers999999() // Зовем побочную функцию
 }
 
 fun WhenTest() {
-    for(schetchik in 1..4)
-        {
-        var variants = readLine()!!.toInt()
-        when (variants) {
-            1 -> println("Привет!")
-            2 -> println("Пока!")
-            3 -> println("Как дела?")
-            else -> println("Иди сюда!")
-        }
+    var variants = readLine()!!.toInt()
+    when (variants) {
+        1 -> println("Привет!")
+        2 -> println("Пока!")
+        3 -> println("Как дела?")
+        else -> println("Иди сюда!")
     }
 }
 
@@ -21,6 +18,7 @@ fun Task1() {
                 "которая в зависимсоти от погоды\n" +
                 "дает совет что лучше делать.\n"
     )
+
     print(
         "1 - снег,\n" +
                 "2 - дождь,\n" +
@@ -31,42 +29,161 @@ fun Task1() {
     )
 
     var Weatherman = readLine()!!.toInt()
-when(Weatherman) {
-    1 -> println("Советник советует посоветовать вам что-бы вы надели шубу и варежки")
-    2 -> println("Советник советует посоветовать вам что-бы вы надели дождевик и взяли зонт")
-    3 -> println("Советник советует посоветовать вам что-бы вы надели футболку и позагарали")
-    4 -> println("Советник советует посоветовать вам что-бы вы надели куртку и ветроловку")
-    5 -> println("Советник советует нифига не делать и играть в майнкрафт")
-    else -> println("Советник советует посоветовать вам что у вас с наружи")
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    when (Weatherman) {
+        1 -> println("Советник советует посоветовать вам что-бы вы надели шубу и варежки")
+        2 -> println("Советник советует посоветовать вам что-бы вы надели дождевик и взяли зонт")
+        3 -> println("Советник советует посоветовать вам что-бы вы надели футболку и позагарали")
+        4 -> println("Советник советует посоветовать вам что-бы вы надели куртку и ветроловку")
+        5 -> println("Советник советует нифига не делать и играть в майнкрафт")
+        else -> println("Советник советует посоветовать вам что у вас с наружи")
+    }
 
 
 }
 
-fun For_Test()
-{
-    print("Введите начало: ")
-    var start = readLine()!!.toInt()
-    print("Введите конец: ")
-    var finish = readLine()!!.toInt()
-    for(num in start..finish)
-    {
-        println(num)
+
+fun Numbers999999() {
+    print(
+        "Case18." +
+                "Дано целое число в диапазоне 1–999999.\n" +
+                "Вывести строку-описание данного числа,\n" +
+                "например:\n" +
+                "256 — «двести пятьдесят шесть»,\n" +
+                "814 — «восемьсот четырнадцать».\n"
+    )
+    while (true) {
+        var string_description = ""
+
+        print("Введите число[1-999999]: ")
+        var numbers = readLine()!!.toInt()
+
+        if (numbers in 1..999999) {
+            println("Все Ок-с!")
+
+
+            //сотни тысяч
+            when (numbers / 100000) {
+                1 -> string_description += "сто "
+                2 -> string_description += "двести "
+                3 -> string_description += "триста "
+                4 -> string_description += "четыреста "
+                5 -> string_description += "пятьсот "
+                6 -> string_description += "шестьсот "
+                7 -> string_description += "семьсот "
+                8 -> string_description += "восемьсот "
+                9 -> string_description += "девятьсот "
+            }
+
+            //десятки тысячи
+            when (numbers % 100000 / 10000) {
+
+                1 ->
+                {
+                    when(numbers%100000/1000%10){
+                        0 -> string_description += "десять "
+                        1 -> string_description += "одинадцать "
+                        2 -> string_description += "двенадцать "
+                        3 -> string_description += "тринадцать "
+                        4 -> string_description += "четырнадцать "
+                        5 -> string_description += "пятнадцать "
+                        6 -> string_description += "шестнадцать "
+                        7 -> string_description += "семнадцать "
+                        8 -> string_description += "восемнадцать "
+                        9 -> string_description += "девятнадцать "
+
+                    }
+                    string_description += "тысяч "
+
+                }
+                2 -> string_description += "двадцать "
+                3 -> string_description += "тридцать "
+                4 -> string_description += "сорок "
+                5 -> string_description += "пятьдесят "
+                6 -> string_description += "шестьдесят "
+                7 -> string_description += "семьдесят "
+                8 -> string_description += "восемьдесят "
+                9 -> string_description += "девяносто "
+            }
+
+            //тысячи
+            if (numbers%100000/1000 in 10..19)
+            {
+
+            }
+            else
+            {
+                when (numbers % 10000 / 1000) {
+                    0->  string_description += "тысяч "
+                    1 -> string_description += "одна тысяча "
+                    2 -> string_description += "две тысячи "
+                    3 -> string_description += "три тысячи "
+                    4 -> string_description += "четыре тысячи "
+                    5 -> string_description += "пять тысяч "
+                    6 -> string_description += "шесть тысяч "
+                    7 -> string_description += "семь тысяч "
+                    8 -> string_description += "восемь тысяч "
+                    9 -> string_description += "девять тысяч "
+                }
+            }
+
+
+            //сотни
+            when (numbers % 1000 / 100) {
+                1 -> string_description += "сто "
+                2 -> string_description += "двести "
+                3 -> string_description += "триста "
+                4 -> string_description += "четыреста "
+                5 -> string_description += "пятьсот "
+                6 -> string_description += "шестьсот "
+                7 -> string_description += "семьсот "
+                8 -> string_description += "восемьсот "
+                9 -> string_description += "девятьсот "
+            }
+
+            //десятки
+            when (numbers % 100 / 10) {
+                1 -> {
+                    when (numbers % 10) {
+                        0 -> string_description += "десять "
+                        1 -> string_description += "одинадцать "
+                        2 -> string_description += "двенадцать "
+                        3 -> string_description += "тринадцать "
+                        4 -> string_description += "четырнадцать "
+                        5 -> string_description += "пятнадцать "
+                        6 -> string_description += "шестнадцать "
+                        7 -> string_description += "семнадцать "
+                        8 -> string_description += "восемнадцать "
+                        9 -> string_description += "девятнадцать "
+                    }
+                }
+                2 -> string_description += "двадцать "
+                3 -> string_description += "тридцать "
+                4 -> string_description += "сорок "
+                5 -> string_description += "пятьдесят "
+                6 -> string_description += "шестьдесят "
+                7 -> string_description += "семьдесят "
+                8 -> string_description += "восемьдесят "
+                9 -> string_description += "девяносто "
+            }
+            //единицы
+            if (numbers%100 in 10..19) {
+            }
+            else {
+                when (numbers % 10) {
+                    1 -> string_description += "один"
+                    2 -> string_description += "два"
+                    3 -> string_description += "три"
+                    4 -> string_description += "четыре"
+                    5 -> string_description += "пять"
+                    6 -> string_description += "шесть"
+                    7 -> string_description += "семь"
+                    8 -> string_description += "восемь"
+                    9 -> string_description += "девять"
+                }
+            }
+        } else {
+            print("А условие кто читать будет? А?")
+        }
+        println(string_description)
     }
 }
-
-
